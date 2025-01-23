682c840fe42e93b04ab07e00ccc943ca3a364a75
CrisCODESPACE <cristina.martin@codespaceacademy.com>
2025-01-23 13:14:57 +0100

    dia 21.1

--- Día 21 LOGIN REGISTER/logSimulation/src/js/welcome.js
+++ Día 21 LOGIN REGISTER/logSimulation/src/js/welcome.js
@@ -15,6 +15,11 @@ function displayToken() {
 displayToken();
 
 document.getElementById("logoutButton").addEventListener("click", () => {
-  sessionStorage.clear();
+  sessionStorage.removeItem("token");
   window.location.href = "index.html";
 });
+
+// Para guardar un token en el Local Storage o en el Session Storage usamos --> localStorage.setItem("key", value);
+// Para obtener un token que se encuentra alojado en el Local Storage o en el Session Storage usamos --> sessionStorage.getItem("keyName");
+// Para limpiar los datos guardados en el local o session usamos --> localStorage.clear();
+// Para borrar un item en concreto del local o el session usamos --> sessionStorage.removeItem("keyName");
